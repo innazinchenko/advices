70ffa1eb3d6ca5c4d22a0a098f635c12c9bea6db
innazinchenko <118972010+innazinchenko@users.noreply.github.com>
2023-02-06 15:16:35 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [advice, setAdvice] = useState("");
 
   const receivedAdvice = async () => {
-  const response = await fetch ("http://www.boredapi.com/api/activity/");
+  const response = await fetch ("https://www.boredapi.com/api/activity/");
   const data = await response.json();
   setAdvice(data.activity);
      };
